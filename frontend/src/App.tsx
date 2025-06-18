a79f24bd918b595f08b8e42a8669d193b242232e
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-06-18 16:39:34 -0700

    Created sample for env files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 function App() {
   const [message, setMessage] = useState<string>("");
 
   useEffect(() => {
     // This endpoint is defined in TestController
-    fetch("http://localhost:8080/api/test")
+    fetch(`${API_BASE_URL}/api/test`)
       .then((response) => response.text())
       .then((data) => setMessage(data))
       .catch((error) => console.error("Error fetching data:", error));
