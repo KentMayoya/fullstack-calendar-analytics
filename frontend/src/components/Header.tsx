371e69e0a6848ad2ae38100d82daf4bbc341f1a2
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-07-07 17:03:27 -0700

    Added a Sign in button on the header when the user is not logged in, and their profile picture when they are logged in. The sign in button currently does not do anything.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
-import { AppBar, Toolbar, IconButton } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Avatar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import LoginIcon from "@mui/icons-material/Login";
 import DrawerMenu from "./DrawerMenu";
+import { useUser } from "../setup/app-context-manager/UserContext";
 
 const Header = () => {
+  const { session } = useUser();
+
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
 
   return (
@@ -17,6 +21,25 @@ const Header = () => {
             onClick={() => setIsDrawerOpen(true)}
           >
             <MenuIcon />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            sx={{
+              marginLeft: "auto",
+              display: "flex",
+              alignItems: "center",
+              gap: 1,
+            }}
+          >
+            {session && (
+              <Avatar src={session?.auth?.user_metadata?.avatar_url}> </Avatar>
+            )}
+            {!session && (
+              <>
+                <Typography>Sign in</Typography>
+                <LoginIcon />
+              </>
+            )}
           </IconButton>
         </Toolbar>
       </AppBar>
