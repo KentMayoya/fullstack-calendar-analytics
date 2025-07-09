735eee678c49e59b195567ceb0f2a8fde369be56
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-07-09 11:31:04 -0700

    Added centered website title to the header that navigates to the homepage upon click

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
-import { AppBar, Toolbar, IconButton } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Button,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import DrawerMenu from "./DrawerMenu";
 import UserMenu from "./UserMenu";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
@@ -19,6 +27,21 @@ const Header = () => {
           >
             <MenuIcon />
           </IconButton>
+          <Box sx={{ flexGrow: 1 }}>
+            <Button
+              component={Link}
+              to="/"
+              sx={{
+                color: "inherit",
+                textTransform: "none",
+                "&:focus, &:active, &:hover": {
+                  color: "inherit",
+                },
+              }}
+            >
+              <Typography variant="h6">Calendar Analytics</Typography>
+            </Button>
+          </Box>
           <UserMenu />
         </Toolbar>
       </AppBar>
