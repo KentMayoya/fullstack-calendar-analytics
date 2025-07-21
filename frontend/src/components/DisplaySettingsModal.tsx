f3ea7846ab651566c6db0dc0cc77751e5bfef471
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-07-21 11:29:47 -0700

    Completed DisplaySettingsModal layout. Visual effects and functionality are not yet implemented

--- frontend/src/components/DisplaySettingsModal.tsx
+++ frontend/src/components/DisplaySettingsModal.tsx
@@ -1,13 +1,122 @@
 import ReusableModal from "./ReusableModal";
+import {
+  Typography,
+  ToggleButtonGroup,
+  ToggleButton,
+  CircularProgress,
+  FormControlLabel,
+  Checkbox,
+  Box,
+  Button,
+} from "@mui/material";
+import { useState } from "react";
+import { useCalendar } from "../hooks/useCalendar";
 
 type DisplaySettingsModalProps = {
   handleClose: () => void;
 };
 
 const DisplaySettingsModal = ({ handleClose }: DisplaySettingsModalProps) => {
+  const { calendars, loading } = useCalendar();
+  const [selectedIds, setSelectedIds] = useState<Set<string>>(new Set());
+
+  // This method is triggered every time a checkbox is clicked. If the triggering
+  // calendarId is already in the set, remove it. Otherwise, add it.
+  const handleSelectionChange = (calendarId: string) => {
+    setSelectedIds((prevSelectedIds) => {
+      const newSelectedIds = new Set(prevSelectedIds);
+      if (newSelectedIds.has(calendarId)) {
+        newSelectedIds.delete(calendarId);
+      } else {
+        newSelectedIds.add(calendarId);
+      }
+      return newSelectedIds;
+    });
+  };
+
   return (
     <ReusableModal isOpen={true} handleClose={handleClose}>
-      <h1>test</h1>
+      {!loading && (
+        <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+          <Typography
+            component="h2"
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+            }}
+          >
+            Calendar View
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "center", p: 2 }}>
+            <ToggleButtonGroup exclusive>
+              <ToggleButton value="week">Week</ToggleButton>
+              <ToggleButton value="day">Day</ToggleButton>
+              <ToggleButton value="list">List</ToggleButton>
+            </ToggleButtonGroup>
+          </Box>
+          <Typography
+            component="h2"
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+              mb: 2,
+            }}
+          >
+            Displayed Calendars
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              overflowY: "auto",
+              maxHeight: 200,
+            }}
+          >
+            {loading && <CircularProgress />}
+            {!loading &&
+              (calendars.length === 0 ? (
+                <Typography
+                  sx={{ color: "text.secondary", textAlign: "center" }}
+                >
+                  No calendars found.
+                </Typography>
+              ) : (
+                // If not empty, map over the array as before
+                calendars.map((calendar) => (
+                  <FormControlLabel
+                    key={calendar.id}
+                    control={
+                      <Checkbox
+                        checked={selectedIds.has(calendar.id)}
+                        onChange={() => handleSelectionChange(calendar.id)}
+                      />
+                    }
+                    label={calendar.name}
+                  />
+                ))
+              ))}
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              mt: 2,
+            }}
+          >
+            <Button variant="contained" color="primary" onClick={handleClose}>
+              Cancel
+            </Button>
+            {calendars.length > 0 ? (
+              <Button variant="contained" color="primary">
+                Save
+              </Button>
+            ) : (
+              <Button variant="contained" color="primary">
+                Settings
+              </Button>
+            )}
+          </Box>
+        </Box>
+      )}
     </ReusableModal>
   );
 };
