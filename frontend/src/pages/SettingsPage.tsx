a50597945a40d2ebf61576a25786522a757bc35f
Kent <151674193+KentMayoya@users.noreply.github.com>
2025-08-06 16:12:52 -0700

    Moved Calendar Sync Settings into a separate file for SRP.

--- frontend/src/pages/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage.tsx
@@ -22,24 +22,13 @@ import InfoOutlineIcon from "@mui/icons-material/InfoOutline";
 import { useUser } from "../setup/app-context-manager/UserContext";
 import { Navigate } from "react-router-dom";
 import { useState } from "react";
-import {
-  useCalendar,
-  type Calendar,
-} from "../setup/app-context-manager/CalendarContext";
 import { useTags } from "../hooks/useTags";
-import UnsyncCalendarModal from "../components/UnsyncCalendarModal";
 import ProfileSettings from "../components/ProfileSettings";
+import CalendarSyncSettings from "../components/CalendarSyncSettings";
 
 const SettingsPage = () => {
   const { session, supabase } = useUser();
   const { tags, fetchTags, isLoadingTags } = useTags();
-
-  // Used when fetching calendars from Google Calendar
-  const [isSyncing, setIsSyncing] = useState<boolean>(false);
-
-  const [calendarToUnsync, setCalendarToUnSync] = useState<Calendar | null>(
-    null
-  );
 
   // Boolean flag to display textbox
   const [isAddingTag, setIsAddingTag] = useState<boolean>(false);
@@ -56,27 +45,8 @@ const SettingsPage = () => {
   // Temporarily stores the edited tags name before saving.
   const [editedTagName, setEditedTagName] = useState<string>("");
 
-  // calendars: Contains a list of calendars from the database
-  // isLoadingCalendars: Used when fetching calendars from the database
-  // updateSyncStatus: Updates the database when a user toggles a switch for
-  // a specified calendar
-  const {
-    setCalendars,
-    calendars,
-    isLoadingCalendars,
-    handleToggleSync: updateSyncStatus,
-  } = useCalendar();
-
-  // Controls the visibility of the Sync Info Dialog
-  const [isSyncInfoDialogOpen, setIsSyncInfoDialogOpen] =
-    useState<boolean>(false);
-
   // Controls the visibility of the Tag Info Dialog
   const [isTagInfoDialogOpen, setIsTagInfoDialogOpen] =
-    useState<boolean>(false);
-
-  // Controls the visibility of the UnsyncCalendarModal
-  const [isUnsyncCalendarModalOpen, setIsUnsyncCalendarModalOpen] =
     useState<boolean>(false);
 
   const [isDeleteAccountDialogOpen, setIsDeleteAccountDialogOpen] =
@@ -90,42 +60,6 @@ const SettingsPage = () => {
   if (!session?.auth) {
     return <Navigate to="/" replace />;
   }
-
-  // Fetches Google Calendars through the Google Calendar API
-  const fetchGoogleCalendars = async () => {
-    setIsSyncing(true);
-    try {
-      const response = await fetch(`${API_BASE_URL}/api/v1/calendars/sync`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${session?.access_token}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Failed to fetch calendars");
-      }
-      const updatedCalendars = await response.json();
-      setCalendars(updatedCalendars);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsSyncing(false);
-    }
-  };
-
-  // If the calendar is to be unsynced, displays the Confirmation modal to
-  // unsync an event and updates the calendar's sync status in the database
-  const handleToggleSync = (calendar: Calendar) => {
-    const { id, isSynced } = calendar;
-    if (!isSynced) {
-      // If turning ON, start the sync.
-      updateSyncStatus(id, isSynced);
-    } else {
-      // If turning OFF, store the calendar and open the confirmation modal.
-      setCalendarToUnSync(calendar);
-      setIsUnsyncCalendarModalOpen(true);
-    }
-  };
 
   // Calls /api/v1/tags endpoint to create a tag
   const handleCreateTag = async () => {
@@ -244,73 +178,7 @@ const SettingsPage = () => {
     <Box sx={{ p: { xs: 1, lg: 3 } }}>
       <ProfileSettings />
       <Divider sx={{ my: 2 }} />
-      <Box sx={{ display: "flex", alignItems: "center" }}>
-        <Typography
-          component="h2"
-          variant="h6"
-          sx={{
-            fontWeight: "bold",
-          }}
-        >
-          Calendar Sync Settings
-        </Typography>
-        <IconButton
-          onClick={() => setIsSyncInfoDialogOpen(true)}
-          aria-label="info"
-        >
-          <InfoOutlineIcon />
-        </IconButton>
-      </Box>
-      <Dialog
-        open={isSyncInfoDialogOpen}
-        onClose={() => setIsSyncInfoDialogOpen(false)}
-      >
-        <DialogTitle>Managing Calendar Sync</DialogTitle>
-        <DialogContent>
-          <Typography>
-            The "Load Calendars" buttons loads your calendars from Google
-            Calendar. Click on the toggle to change the calendar's sync status.
-            Marking a calendar as syncable schedules an hourly job to sync your
-            events. However, this does not trigger an immediate sync. To
-            manually trigger a sync, view "Sync Settings" on the calendar page.
-          </Typography>
-        </DialogContent>
-      </Dialog>
-      {(isLoadingCalendars || isSyncing) && (
-        <Box>
-          <CircularProgress />
-        </Box>
-      )}
-      {!isLoadingCalendars && !isSyncing && (
-        <List>
-          {calendars.map((calendar) => (
-            <ListItem key={calendar.id} sx={{ py: 0 }}>
-              <ListItemText primary={calendar.name} />
-              <Switch
-                edge="end"
-                checked={calendar.isSynced ?? false}
-                onChange={() => handleToggleSync(calendar)}
-              />
-            </ListItem>
-          ))}
-        </List>
-      )}
-      {isUnsyncCalendarModalOpen && calendarToUnsync && (
-        <UnsyncCalendarModal
-          handleClose={() => {
-            setIsUnsyncCalendarModalOpen(false);
-            setCalendarToUnSync(null);
-          }}
-          calendar={calendarToUnsync}
-        ></UnsyncCalendarModal>
-      )}
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={fetchGoogleCalendars}
-      >
-        Load Calendars
-      </Button>
+      <CalendarSyncSettings />
       <Divider sx={{ my: 2 }} />
       <Box sx={{ display: "flex", alignItems: "center" }}>
         <Typography
